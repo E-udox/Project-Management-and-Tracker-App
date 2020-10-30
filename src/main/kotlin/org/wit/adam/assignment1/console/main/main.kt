a5910205ce09fb38d18b24e9a042781c66528e50
E-udox <20076745@mail.wit.ie>
2020-10-30 20:08:31 +0000

    Project structure and main.kt setup.

---
+++ src/main/kotlin/org/wit/adam/assignment1/console/main/main.kt
@@ -0,0 +1,8 @@
+package org.wit.adam.assignment1.console.main
+import mu.KotlinLogging
+
+val logger = KotlinLogging.logger {}
+
+fun main(args: Array<String>) {
+    logger.info { "Launching Project Management and Tracker App (Console Version)" }
+}
